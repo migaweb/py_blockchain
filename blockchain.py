4a0a7af7491e8e3038f22d4924f13364b74acc80
micke <miga02@gmail.com>
2020-10-05 19:45:09 +0200

    first commit

---
+++ blockchain.py
@@ -0,0 +1,28 @@
+from block import Block
+
+
+class Blockchain:
+    """
+    Blockchain: a public ledger of transactions.
+    Implemented as a list of blocks - datasets of transactions.
+    """
+
+    def __init__(self):
+        self.chain = [Block.genesis()]
+
+    def add_block(self, data):
+        self.chain.append(Block.mine_block(self.chain[-1], data))
+
+    def __repr__(self):
+        return f'Blockchain: {self.chain}'
+
+
+def main():
+    bc = Blockchain()
+    bc.add_block('one')
+    bc.add_block('two')
+    print(bc)
+
+
+if (__name__ == '__main__'):
+    main()
