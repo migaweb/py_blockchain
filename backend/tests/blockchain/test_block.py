71ab96859592aff12b6c35d1b586fd738cee921e
micke <miga02@gmail.com>
2020-10-09 19:44:47 +0200

    Added block validation

--- backend/tests/blockchain/test_block.py
+++ backend/tests/blockchain/test_block.py
@@ -1,4 +1,5 @@
 import time
+import pytest
 from backend.blockchain.block import Block, GENESIS_DATA
 from backend.config import MINE_RATE, SECONDS
 from backend.util.hex_to_binary import hex_to_binary
@@ -45,6 +46,49 @@ def test_mine_block_difficulty_limits_at_1():
     mined_block = Block.mine_block(last_block, 'bar')
 
     assert mined_block.difficulty == 1
+
+
+@pytest.fixture
+def last_block():
+    return Block.genesis()
+
+
+@pytest.fixture
+def block(last_block):
+    return Block.mine_block(last_block, 'test-data')
+
+
+def test_is_valid_block(last_block, block):
+    Block.is_valid_block(last_block, block)
+
+
+def test_is_valid_block_bad_last_hash(last_block, block):
+    block.last_hash = 'evil_last_hash'
+
+    with pytest.raises(Exception, match='last_hash must be correct'):
+        Block.is_valid_block(last_block, block)
+
+
+def test_is_valid_block_bad_proof_of_work(last_block, block):
+    block.hash = 'fff'
+    with pytest.raises(Exception, match='proof of work requirement was not met'):
+        Block.is_valid_block(last_block, block)
+
+
+def test_is_valid_block_jumped_difficulty(last_block, block):
+    jumped_difficulty = 10
+    block.difficulty = jumped_difficulty
+    block.hash = f'{"0" * jumped_difficulty}11a77468abc'
+
+    with pytest.raises(Exception, match='difficulty must only adjust by 1'):
+        Block.is_valid_block(last_block, block)
+
+
+def test_is_valid_block_bad_block_hash(last_block, block):
+    block.hash = '000000000000000000000000abc1234'
+
+    with pytest.raises(Exception, match='block hash must be correct'):
+        Block.is_valid_block(last_block, block)
 
 
 def test_genesis():
